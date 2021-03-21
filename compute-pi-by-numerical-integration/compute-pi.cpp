b21bf26e3ff8534fb1468bf113676abf66732979
Miguel Ferreira <miguel.correiaferreira@gmail.com>
2021-03-21 01:31:57 +0000

    Added some improvements

--- compute-pi-by-numerical-integration/compute-pi.cpp
+++ compute-pi-by-numerical-integration/compute-pi.cpp
@@ -4,20 +4,21 @@
 #include <ostream>
 #include <pthread.h>
 #include <stdio.h>
-#define NUM_RECT 1000000000
-#define NUMTHREADS 4
+long numRectangles = 1000000000;
+int numThreads = 4;
 double integralMinimum = 0.0;
 double integralMaximum = 1.0;
 double pi = 0.0;
 pthread_mutex_t gLock;
 
-void *Area(void *pArg)
+// thread function that calculates the area of the rectangles and sums them
+void *calcAreasAndSum(void *pArg)
 {
     int myNum = *((int *)pArg);
-    double step = integralMaximum / NUM_RECT;
+    double step = integralMaximum / numRectangles;
     double partialSum = 0.0, x, y;
 
-    for (int i = myNum; i < NUM_RECT; i += NUMTHREADS)
+    for (int i = myNum; i < numRectangles; i += numThreads)
     {
         x = i * step;
         y = 4 / (1 + x * x);
@@ -29,29 +30,55 @@ void *Area(void *pArg)
     return 0;
 }
 
+void setArgs(int argNumThreads, long argNumRectangles = 0)
+{
+    if (argNumThreads > 0)
+    {
+        numThreads = argNumThreads;
+    }
+    if (argNumRectangles > 0)
+    {
+        numRectangles = argNumRectangles;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+    // arguments: #1 is number of threads, #2 is number of rectangles.
+    if (argc == 2)
+    {
+        setArgs(std::atoi(argv[1]));
+    }
+    else if (argc == 3)
+    {
+        setArgs(std::atoi(argv[1]), std::atol(argv[2]));
+    }
+
     // start measuring the execution time
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-    pthread_t tHandles[NUMTHREADS];
-    int tNum[NUMTHREADS], i;
+    pthread_t tHandles[numThreads];
+    int tNum[numThreads], i;
     pthread_mutex_init(&gLock, NULL);
-    for (i = 0; i < NUMTHREADS; ++i)
+
+    // create the threads
+    for (i = 0; i < numThreads; ++i)
     {
         tNum[i] = i;
-        pthread_create(&tHandles[i],      // Returned thread handle
-                       NULL,              // Thread Attributes
-                       Area,              // Thread function
-                       (void *)&tNum[i]); // Data for Area()
+        pthread_create(&tHandles[i],      // returned thread handle
+                       NULL,              // thread attributes
+                       calcAreasAndSum,   // thread function
+                       (void *)&tNum[i]); // data for calcAreasAndSum()
     }
-    for (i = 0; i < NUMTHREADS; ++i)
+
+    // wait for the created threads to terminate
+    for (i = 0; i < numThreads; ++i)
     {
         pthread_join(tHandles[i], NULL);
     }
 
-    std::cout << "Multithreaded calculation using " << NUMTHREADS << " threads\n";
-    std::cout << "Real value of Pi: 3.1415926535897932384626433\n";
+    std::cout << "Multithreaded calculation using " << numThreads << " threads (" << numRectangles << " Rectangles)" << std::endl;
+    std::cout << "Real value of Pi: 3.1415926535897932384626433" << std::endl;
     printf("Calc value of Pi: %26.25f\n", pi);
     pthread_mutex_destroy(&gLock);
 
