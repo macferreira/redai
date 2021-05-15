46fa6a9a5c18b1cd39e8b30402569af0b7ab431b
Miguel Ferreira <miguel.correiaferreira@gmail.com>
2021-05-15 22:41:51 +0100

    Fix generate points

--- convex-hull-graham-scan/01-generate-points/generate-points.cpp
+++ convex-hull-graham-scan/01-generate-points/generate-points.cpp
@@ -3,41 +3,49 @@
 #include <iostream>
 #include <ostream>
 #include <stdio.h>
+#include <vector>
 
-int points;
-double maxXY;
-std::string fileName = "original.dat";
+struct Point
+{
+    double x, y;
+};
+
+std::vector<Point> points;
+
+int totalPoints;
+int maxXY;
+std::string fileName;
 
 int main(int argc, char *argv[])
 {
-    // arguments: #1 is n number of points, #2 the max x and y value.
-    if (argc == 3)
+    // arguments:
+    //     #1 is n number of points,
+    //     #2 is the max x and y value,
+    //     #3 is the name of the output file.
+    if (argc == 4)
     {
-        points = std::atoi(argv[1]);
-        maxXY = std::stod(argv[2]);
+        totalPoints = std::atoi(argv[1]);
+        maxXY = std::atoi(argv[2]);
+        fileName = argv[3];
     }
     else
     {
-        std::cout << "Usage: " << argv[0] << " <number of points> <max x and y value>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <number of points> <max x and y value> <output file>" << std::endl;
     }
 
-    double x, y;
-    double arrX[100];
-    double arrY[100];
-
     double min = 0.0;
-    double max = maxXY;
+    double max = maxXY*1.0;
 
     /**
      * generate random coordinates
      */
-    for (int i = 0; i < points; i++)
+    Point p;
+    for (int i = 0; i < totalPoints; i++)
     {
-        x = (max - min) * ( (double)rand() / (double)RAND_MAX ) + min;
-        y = (max - min) * ( (double)rand() / (double)RAND_MAX ) + min;
+        p.x = (max - min) * ((double)rand() / (double)RAND_MAX) + min;
+        p.y = (max - min) * ((double)rand() / (double)RAND_MAX) + min;
 
-        arrX[i] = x;
-        arrY[i] = y;
+        points.push_back(p);
     }
 
     /**
@@ -47,12 +55,12 @@ int main(int argc, char *argv[])
     datFile.open(fileName);
 
     // first line of the file is the total number of points
-    datFile << points << std::endl;
+    //datFile << points << std::endl;
 
-    // iterate the aray to write all the coordinates
-    for (int i = 0; i < points; i++)
+    // iterate the vector to write all the coordinates
+    for (int i = 0; i < points.size(); i++)
     {
-        datFile << arrX[i] << "," << arrY[i] << std::endl;
+        datFile << points[i].x << "," << points[i].y << std::endl;
     }
 
     datFile.close();
